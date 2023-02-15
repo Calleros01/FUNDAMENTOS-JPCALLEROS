910d1c58940921458cfec806746f739d908c0478
Toysabroso <125276755+Calleros01@users.noreply.github.com>
2023-02-15 08:49:20 -0600

    001_JPCALLEROS
    Primer práctica realizada en Visual Studio

---
+++ 2023_02_15_001_HolaMundo_V0/2023_02_15_001_HolaMundo_V0.cpp
@@ -0,0 +1,20 @@
+// 2023_02_15_001_HolaMundo_V0.cpp 
+// ESTE ARCHIVO ES MI PRIMER PROGRAMA
+//Juan Pablo Calleros Gudiño zap903
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "WELCOME TO MY WORLD\n";
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
